480ab87c9a09ce419a4a0381aab59d69705db042
sothearo19 <kay.sothearo@outlook.com>
2024-09-04 09:01:17 +0700

    feat(*): scroll to top when new element has been added

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
 import { TodoListItemComponent } from '../todo-list-item/todo-list-item.component';
 import { AddTodoComponent } from '../add-todo/add-todo.component';
 import { DeleteTodoComponent } from '../delete-todo/delete-todo.component';
@@ -11,18 +11,26 @@ import { DeleteTodoComponent } from '../delete-todo/delete-todo.component';
   styleUrl: './todo-list.component.scss',
 })
 export class TodoListComponent {
+  @ViewChild('todoList') todoListRef!: ElementRef<HTMLUListElement>;
   todos = [
     { id: 1, title: 'Learn Angular', completed: false },
     { id: 2, title: 'Build a Todo App', completed: false },
     { id: 3, title: 'Write Documentation', completed: true },
   ];
 
+  constructor(private renderer: Renderer2) {}
+
   get completedTodoCount(): number {
     return this.todos.filter((todo) => todo.completed).length;
   }
 
   addTodo(title: string): void {
-    this.todos.push({ id: this.todos.length + 1, title, completed: false });
+    this.todos.unshift({ id: this.todos.length + 1, title, completed: false });
+
+    // set the scroll position to the top
+    setTimeout(() => {
+      this.todoListRef.nativeElement.scrollTop = 0;
+    }, 0);
   }
 
   deleteTodo(): void {
